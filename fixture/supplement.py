0d7de1aa7f60471bd2cd0460735bae8dc533543e
Falschberg <oleksandr.davydiuk@gmail.com>
2016-05-25 17:46:34 +0300

    Выделен класс для работы с группами

--- fixture/supplement.py
+++ fixture/supplement.py
@@ -1,102 +1,19 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.new_contact import NewContactHelper
 
 class Supplement:
+
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.new_contact = NewContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def create_new_contact(self, new_contact):
-        wd = self.wd
-        # init new contact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill new contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(new_contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(new_contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(new_contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(new_contact.nickname)
-        wd.find_element_by_name("photo").send_keys(new_contact.photo)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(new_contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(new_contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(new_contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(new_contact.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(new_contact.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(new_contact.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(new_contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(new_contact.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(new_contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(new_contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(new_contact.homepage)
-        wd.find_element_by_xpath(new_contact.bday).click()
-        wd.find_element_by_xpath(new_contact.bmonth).click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(new_contact.byear)
-        wd.find_element_by_xpath(new_contact.aday).click()
-        wd.find_element_by_xpath(new_contact.amonth).click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(new_contact.ayear)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(new_contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(new_contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(new_contact.notes)
-        # submite new contact creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_to_home_page()
-
-    def create_empty_contact(self):
-        wd = self.wd
-        # init new contact creation
-        wd.find_element_by_link_text("add new").click()
-        # submite new contact creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_to_home_page()
-
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -4,7 +4,7 @@ from fixture.supplement import Supplement
 from model.new_contact import New_contact
 
 
-@pytest.fixture
+@pytest.fixture()
 def app(request):
     fixture = Supplement()
     request.addfinalizer(fixture.destroy)
@@ -13,18 +13,18 @@ def app(request):
 
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(New_contact(firstname="Louis", middlename="van", lastname="Gaal", nickname="King Louis", title="Coach", company="Manchester United Football Club",
-                                            address="Old Trafford", home="+44 (0) 161 868 8000", mobile="+44 (0) 161 868 8000", work="+44 (0) 161 868 8000", fax="+44 (0) 161 868 8000",
-                                            email="membership@manutd.co.uk", email2="tickets@manutd.co.uk", email3="disability@manutd.co.uk", homepage="http://www.manutd.com/",
-                                            byear="1951", ayear="2026", address2="Sir Matt Busby Way, Manchester, M16 0RA", phone2="+44 (0) 161 868 8000",
-                                            notes="Aloysius Paulus Maria van Gaal", bday="//div[@id='content']/form/select[1]//option[10]", bmonth="//div[@id='content']/form/select[2]//option[9]",
-                                            aday="//div[@id='content']/form/select[3]//option[10]", amonth="//div[@id='content']/form/select[4]//option[9]",
-                                            photo="C:\\Users\\FalschBerg\\Desktop\\Louis_van_Gaal_2013.jpg"))
+    app.new_contact.create(New_contact(firstname="Louis", middlename="van", lastname="Gaal", nickname="King Louis", title="Coach", company="Manchester United Football Club",
+                                       address="Old Trafford", home="+44 (0) 161 868 8000", mobile="+44 (0) 161 868 8000", work="+44 (0) 161 868 8000", fax="+44 (0) 161 868 8000",
+                                       email="membership@manutd.co.uk", email2="tickets@manutd.co.uk", email3="disability@manutd.co.uk", homepage="http://www.manutd.com/",
+                                       byear="1951", ayear="2026", address2="Sir Matt Busby Way, Manchester, M16 0RA", phone2="+44 (0) 161 868 8000",
+                                       notes="Aloysius Paulus Maria van Gaal", bday="//div[@id='content']/form/select[1]//option[10]", bmonth="//div[@id='content']/form/select[2]//option[9]",
+                                       aday="//div[@id='content']/form/select[3]//option[10]", amonth="//div[@id='content']/form/select[4]//option[9]",
+                                       photo="C:\\Users\\FalschBerg\\Desktop\\Louis_van_Gaal_2013.jpg"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_empty_contact()
+    app.new_contact.create_empty()
     app.session.logout()
 
