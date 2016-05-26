ce05951c24c7688f33cd8095a8418dcd9827ee59
Falschberg <oleksandr.davydiuk@gmail.com>
2016-05-26 14:04:56 +0300

    Сделана общая фикстура для всех тестов

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.supplement import Supplement
 from model.new_contact import New_contact
-
-
-@pytest.fixture()
-def app(request):
-    fixture = Supplement()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_new_contact(app):
