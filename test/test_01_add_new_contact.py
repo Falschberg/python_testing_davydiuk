5291124f5b9b7d30c3044c541efbc4687aaa3099
Falschberg <oleksandr.davydiuk@gmail.com>
2016-07-02 13:37:12 +0300

    Задание №15: Параметризовать тесты для создания групп и контактов

--- test/test_01_add_new_contact.py
+++ test/test_01_add_new_contact.py
@@ -5,19 +5,19 @@ import random
 import string
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*100
+    symbols = string.ascii_letters + string.digits
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 def random_numbers(maxlen):
     symbols = string.digits
-    return "".join([random.choice(symbols) for i in range(random.randrange(1, maxlen, 1))])
+    return "".join([random.choice(symbols) for i in range(random.randrange(5, maxlen, 1))])
 
 testdata = [Contact(firstname="", lastname="", address="")] + [
     Contact(firstname=random_string("first", 3), middlename=random_string("middle", 3), lastname=random_string("last", 3), nickname=random_string("nick", 3),
             title=random_string("title", 3), company=random_string("company", 3), address=random_string("address", 3),
-            homephone=random_numbers(5), mobilephone=random_numbers(5), workphone=random_numbers(5), faxphone=random_numbers(5),
+            homephone=random_numbers(15), mobilephone=random_numbers(15), workphone=random_numbers(15), faxphone=random_numbers(15),
             email=random_string("email", 3), email2=random_string("email2", 3), email3=random_string("email3", 3), homepage=random_string("homepage", 3),
-            address2=random_string("address2", 3), secondaryphone=random_numbers(5), notes=random_string("notes", 3))
+            address2=random_string("address2", 3), secondaryphone=random_numbers(15), notes=random_string("notes", 3))
     for i in range(3)
 ]
 
--- test/test_02_phones.py
+++ test/test_02_phones.py
@@ -3,12 +3,28 @@ from model.contact import Contact
 import re
 
 def test_phones_on_home_page(app):
+    if app.contact.count() == 0:
+        app.contact.create(
+            Contact(firstname="Firstname", middlename="Middlename", lastname="Lastname", nickname="Nickname",
+                    title="Title", company="Company", address="Address",
+                    homephone="+11111", mobilephone="(22)222", workphone="3-33-33", faxphone="44444",
+                    email="email@email.com", email2="email2@email.com", email3="email3@email.com",
+                    homepage="www.homepage.com",
+                    address2="Address2", secondaryphone="5 55 55", notes="Notes"))
     contact_from_home_page = app.contact.get_contacts_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
+    if app.contact.count() == 0:
+        app.contact.create(
+            Contact(firstname="Firstname", middlename="Middlename", lastname="Lastname", nickname="Nickname",
+                    title="Title", company="Company", address="Address",
+                    homephone="+11111", mobilephone="(22)222", workphone="3-33-33", faxphone="44444",
+                    email="email@email.com", email2="email2@email.com", email3="email3@email.com",
+                    homepage="www.homepage.com",
+                    address2="Address2", secondaryphone="5 55 55", notes="Notes"))
     contact_from_view_page = app.contact.get_contacts_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
