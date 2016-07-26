deef6439c2864dc24c809a69811cbf0197bd97b5
Falschberg <oleksandr.davydiuk@gmail.com>
2016-07-26 14:41:37 +0300

    Задание №21 Final

--- model/contact.py
+++ model/contact.py
@@ -38,7 +38,7 @@ class Contact:
                                    self.email3, self.homepage, self.address2, self.secondaryphone, self.notes, self.all_emails_from_home_page, self.all_phones_from_home_page)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname and self.address == other.address and self.all_phones_from_home_page == other.all_phones_from_home_page and self.all_emails_from_home_page == other.all_emails_from_home_page
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname and (self.address is None or other.address is None or self.address == other.address) and (self.all_phones_from_home_page is None or other.all_phones_from_home_page is None or self.all_phones_from_home_page == other.all_phones_from_home_page) and (self.all_emails_from_home_page is None or other.all_emails_from_home_page is None or self.all_emails_from_home_page == other.all_emails_from_home_page)
 
     def id_or_max(self):
         if self.id:
